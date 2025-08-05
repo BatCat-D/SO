1fb7ea8daa17996bf0cbe92b407b897397257521
BatCat-D <bda791152@gmail.com>
2025-08-05 23:23:22 +0300

    اخر تعديل على الواجهة فورم 2

--- SO/Form2.Designer.cs
+++ SO/Form2.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel16 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnShow = new System.Windows.Forms.Button();
@@ -36,26 +35,29 @@
             this.btnAdd = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.btnR = new System.Windows.Forms.Button();
             this.Changes = new System.Windows.Forms.Label();
             this.labWelcome = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labExit = new System.Windows.Forms.Label();
             this.panel16.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::SO.Properties.Resources._20250309_032255;
-            this.pictureBox1.Location = new System.Drawing.Point(969, 550);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(73, 52);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
             // 
             // panel16
             // 
@@ -69,7 +71,7 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(166)))), ((int)(((byte)(60)))));
-            this.panel2.Location = new System.Drawing.Point(0, 12);
+            this.panel2.Location = new System.Drawing.Point(0, 13);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1094, 10);
             this.panel2.TabIndex = 5;
@@ -78,7 +80,7 @@
             // 
             this.btnShow.Font = new System.Drawing.Font("Cairo", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnShow.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(166)))), ((int)(((byte)(60)))));
-            this.btnShow.Location = new System.Drawing.Point(799, 426);
+            this.btnShow.Location = new System.Drawing.Point(802, 426);
             this.btnShow.Name = "btnShow";
             this.btnShow.Size = new System.Drawing.Size(131, 48);
             this.btnShow.TabIndex = 4;
@@ -89,7 +91,7 @@
             // 
             this.btnChange.Font = new System.Drawing.Font("Cairo", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnChange.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(166)))), ((int)(((byte)(60)))));
-            this.btnChange.Location = new System.Drawing.Point(568, 426);
+            this.btnChange.Location = new System.Drawing.Point(571, 426);
             this.btnChange.Name = "btnChange";
             this.btnChange.Size = new System.Drawing.Size(131, 48);
             this.btnChange.TabIndex = 4;
@@ -100,7 +102,7 @@
             // 
             this.btnAdd.Font = new System.Drawing.Font("Cairo", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAdd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(166)))), ((int)(((byte)(60)))));
-            this.btnAdd.Location = new System.Drawing.Point(333, 426);
+            this.btnAdd.Location = new System.Drawing.Point(334, 426);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(131, 48);
             this.btnAdd.TabIndex = 4;
@@ -125,21 +127,11 @@
             this.panel3.Size = new System.Drawing.Size(144, 135);
             this.panel3.TabIndex = 1;
             // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::SO.Properties.Resources.Icon;
-            this.pictureBox3.Location = new System.Drawing.Point(10, 9);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(123, 116);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 5;
-            this.pictureBox3.TabStop = false;
-            // 
             // btnR
             // 
             this.btnR.Font = new System.Drawing.Font("Cairo", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnR.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(166)))), ((int)(((byte)(60)))));
-            this.btnR.Location = new System.Drawing.Point(90, 426);
+            this.btnR.Location = new System.Drawing.Point(93, 426);
             this.btnR.Name = "btnR";
             this.btnR.Size = new System.Drawing.Size(131, 48);
             this.btnR.TabIndex = 4;
@@ -151,7 +143,7 @@
             this.Changes.AutoSize = true;
             this.Changes.Font = new System.Drawing.Font("Cairo", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Changes.ForeColor = System.Drawing.Color.White;
-            this.Changes.Location = new System.Drawing.Point(439, 561);
+            this.Changes.Location = new System.Drawing.Point(431, 551);
             this.Changes.Name = "Changes";
             this.Changes.Size = new System.Drawing.Size(165, 32);
             this.Changes.TabIndex = 5;
@@ -162,11 +154,108 @@
             this.labWelcome.AutoSize = true;
             this.labWelcome.Font = new System.Drawing.Font("Cairo", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labWelcome.ForeColor = System.Drawing.Color.White;
-            this.labWelcome.Location = new System.Drawing.Point(422, 189);
+            this.labWelcome.Location = new System.Drawing.Point(411, 192);
             this.labWelcome.Name = "labWelcome";
             this.labWelcome.Size = new System.Drawing.Size(193, 32);
-            this.labWelcome.TabIndex = 5;
+            this.labWelcome.TabIndex = 21;
             this.labWelcome.Text = "مرحباً في نظام الكنترول";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cairo", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(403, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 32);
+            this.label1.TabIndex = 21;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Image = global::SO.Properties.Resources.file_00000000420061fab81584fff9ec1412;
+            this.pictureBox6.Location = new System.Drawing.Point(54, 224);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(208, 193);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = global::SO.Properties.Resources.file_000000005ee061f99be5aae5aa7f376c;
+            this.pictureBox5.Location = new System.Drawing.Point(295, 227);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(208, 193);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.Image = global::SO.Properties.Resources.file_000000001af061f5b7c675bf2bd22313;
+            this.pictureBox7.Location = new System.Drawing.Point(452, 480);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(123, 78);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 5;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::SO.Properties.Resources.file_00000000b89461f98d19c1488291b056;
+            this.pictureBox4.Location = new System.Drawing.Point(529, 227);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(208, 193);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::SO.Properties.Resources.file_000000004f4861f9a27f91785f7e5363;
+            this.pictureBox2.Location = new System.Drawing.Point(764, 227);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(208, 193);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::SO.Properties.Resources.Icon;
+            this.pictureBox3.Location = new System.Drawing.Point(10, 9);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(123, 116);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SO.Properties.Resources._20250309_032255;
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 533);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(104, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labExit
+            // 
+            this.labExit.AutoSize = true;
+            this.labExit.BackColor = System.Drawing.Color.White;
+            this.labExit.Font = new System.Drawing.Font("Cairo", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(166)))), ((int)(((byte)(60)))));
+            this.labExit.Location = new System.Drawing.Point(975, 551);
+            this.labExit.Name = "labExit";
+            this.labExit.Size = new System.Drawing.Size(52, 32);
+            this.labExit.TabIndex = 22;
+            this.labExit.Text = "خروج";
             // 
             // Form2
             // 
@@ -175,6 +264,13 @@
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(166)))), ((int)(((byte)(60)))));
             this.ClientSize = new System.Drawing.Size(1039, 602);
+            this.Controls.Add(this.labExit);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.labWelcome);
             this.Controls.Add(this.Changes);
             this.Controls.Add(this.btnR);
@@ -191,11 +287,16 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "الكنترول";
             this.Load += new System.EventHandler(this.Form2_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel16.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,5 +316,12 @@
         private System.Windows.Forms.Button btnR;
         private System.Windows.Forms.Label Changes;
         private System.Windows.Forms.Label labWelcome;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label labExit;
     }
 }
